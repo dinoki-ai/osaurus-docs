baa0bcaf7ef4def229a786674f0e1bcdb47432c3
Terence Pae <terencepae@gmail.com>
2025-12-13 14:11:48 -0800

    update favicon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 const config: Config = {
   title: "Osaurus Docs",
   tagline: "Native Apple Silicon local LLM server",
-  favicon: "img/favicon.ico",
+  favicon: "img/osaurus.svg",
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
